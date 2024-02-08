07f95e97dc3a76f3c13967afc08b30b7744b1694
Vasilii Ianikeev <vasilii.ianikeev@opensynergy.com>
2024-02-08 11:52:07 +0100

    configs/plugins: rust-tools, rustaceanvim, toggleterm

--- lua/configs/plugins/rust-tools.lua
+++ lua/configs/plugins/rust-tools.lua
@@ -6,10 +6,18 @@ return {
         rt.setup({
             server = {
                 on_attach = function(_, bufnr)
+                    local opts = { noremap = true, silent = true }
+                    local keymap = vim.keymap -- for conciseness
+
+                    opts.buffer = bufnr
                     -- Hover actions
                     vim.keymap.set("n", "<Leader>k", rt.hover_actions.hover_actions, { buffer = bufnr })
                     -- Code action groups
                     vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                    keymap.set("n", "<leader>md", vim.lsp.buf.declaration, opts) -- go to declaration
+                    keymap.set("n", "<leader>me", vim.diagnostic.setloclist) -- show error list
+                    keymap.set("n", "<leader>mr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+                    keymap.set("n", "<leader>mD", vim.diagnostic.open_float, opts) -- show diagnostics for line
                 end,
             },
             tools = {
